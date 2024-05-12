c18aff4bd6bdcfa6a978725ea523788ce2edc116
Fred Liu <fredliu876@gmail.com>
2024-05-12 19:37:59 -0400

    Add new videos for demoing agents

--- Home.py
+++ Home.py
@@ -8,11 +8,21 @@ agent, manager = st.tabs(["Agent 🕵️‍♂️", "Manager 👨‍💼"])
 with agent:
     st.write("The agent is a language model that can determine within your chat when to call functions to help you do things automatically!")
     st.write("You can ask it to edit files for you, ask about the weather today (Which stumps all LLMs) etc.")
-    st.video('Weather Today.mp4')
+    st.write("")
+    st.write("Asking the agent about the weather:")
+    st.video("weather_today.mp4")
+    st.write("")
+    st.write("Asking the agent to make meaningful git commits for each file changed:")
+    st.video("making_meaningful_git_commits_for_each_file.mp4")
     st.success("Works well with gpt-3.5-turbo")
 
 with manager:
     st.warning("The agent manager is very experimental: It is an additional language model that manages the agent, in an attempt to give you better results on very broad tasks with minimal user input.")
-    st.error("It's essentially an agent with gpt-3.5-turbo")
+    
+    st.write("")
+    st.write("Asking the agent to make meaningful git commits for each file changed:")
+    st.video("manager_instructs_agent_to_add_video.mp4")
+
+    st.error("If you use gpt-3.5-turbo, this is essentially a regular AI agent")
     st.success("Works somewhat well with gpt-4-turbo")
     st.success("Could use some fine tuning to be even better!")
