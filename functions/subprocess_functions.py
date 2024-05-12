46cff3d7351a7cd1e687e2982e8081dfa4047ce8
Fred Liu <fredliu876@gmail.com>
2024-05-12 18:56:55 -0400

    Fix subprocess functions

--- functions/subprocess_functions.py
+++ functions/subprocess_functions.py
@@ -11,10 +11,6 @@ def run_bash_command(command: str) -> str:
 
     try:
         result = subprocess.run(shlex.split(command), shell=True, check=True, stderr=subprocess.PIPE, text=True, encoding="utf-8")
-        return result.stderr
+        return result.stdout
     except subprocess.CalledProcessError as e:
-        print("HERE" + str(e) + str(e.stderr))
-
-
-print(run_bash_command("git commit -m 'Update files'"))
-    
+        raise subprocess.CalledProcessError(e.stderr)
